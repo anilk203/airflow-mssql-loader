470617edf7f571823ea306c8d7106de65a86d74e
anilk203 <anilk203@gmail.com>
2020-04-17 22:01:43 -0400

    added docker-compose and docker installations

--- install-dependencies.sh
+++ install-dependencies.sh
@@ -1,1 +1,25 @@
 #!/usr/bin/env bash
+
+echo "Install dependencies"
+
+curl -L "https://github.com/docker/compose/releases/download/1.25.5/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+
+chmod +x /usr/local/bin/docker-compose
+
+ln -s /usr/local/bin/docker-compose /usr/bin/docker-compose
+
+docker-compose --version
+
+
+
+sudo yum install -y yum-utils device-mapper-persistent-data lvm2
+sudo yum-config-manager --add-repo https://download.docker.com/linux/centos/docker-ce.repo
+sudo yum install docker-ce
+sudo usermod -aG docker $(whoami)
+sudo systemctl enable docker.service
+sudo systemctl start docker.service
+
+
+docker info
+
+docker network create airflow-mssql-loader
